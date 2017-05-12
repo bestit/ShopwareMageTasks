5828380b4518d4e4acf53985491082565b1ac357
Ahmad El-Bardan <el-bardan@bestit-online.de>
2017-05-12 11:16:04 +0200

    Replace placeholders in dev_env

--- src/Deploy/ReplacePlaceHoldersTask.php
+++ src/Deploy/ReplacePlaceHoldersTask.php
@@ -44,15 +44,33 @@ class ReplacePlaceHoldersTask extends AbstractTask
 
         $pathToRoot = $this->runtime->getEnvOption('from', '.');
         $pathToConfig = "{$pathToRoot}/configs/config_{$environment}.php";
+        $pathToDevScript = "{$pathToRoot}/script/remote/dev_environment.sh";
 
         $parameters = ['DB_HOST', 'DB_NAME', 'DB_PASS', 'DB_PORT', 'DB_USER'];
 
+        /* Replace config placeholder values. */
         foreach ($parameters as $parameter) {
             $cmd = sprintf(
                 'sed -i "s/%s/%s/g" %s',
                 "{$upperEnvironment}_{$parameter}", // Search
                 getenv("{$upperEnvironment}_{$parameter}"), // Replace
                 $pathToConfig // File path
+            );
+
+            $process = $this->runtime->runLocalCommand($cmd);
+
+            if (!$process->isSuccessful()) {
+                return false;
+            }
+        }
+
+        /* Replace dev_environment.sh placeholder values. */
+        foreach ($parameters as $parameter) {
+            $cmd = sprintf(
+                'sed -i "s/%s/%s/g" %s',
+                "VAGRANT_{$parameter}", // Search
+                getenv("VAGRANT_{$parameter}"), // Replace
+                $pathToDevScript // File path
             );
 
             $process = $this->runtime->runLocalCommand($cmd);
